0b4840221335bd26eb780bc5bfa81fe02d1e663e
Dominic Frei <dominic@dominicfrei.com>
2015-12-07 20:50:00 +0100

    Initial Commit

---
+++ JsonPatchSwift/JsonPatchSwift.h
@@ -0,0 +1,19 @@
+//
+//  JsonPatchSwift.h
+//  JsonPatchSwift
+//
+//  Created by Dominic Frei on 07/12/2015.
+//  Copyright © 2015 Dominic Frei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JsonPatchSwift.
+FOUNDATION_EXPORT double JsonPatchSwiftVersionNumber;
+
+//! Project version string for JsonPatchSwift.
+FOUNDATION_EXPORT const unsigned char JsonPatchSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JsonPatchSwift/PublicHeader.h>
+
+
