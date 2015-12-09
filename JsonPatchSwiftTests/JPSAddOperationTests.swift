ea3819b91736242b94fbdbec56a4bc504a65b340
Dominic Frei <dominic@dominicfrei.com>
2015-12-09 16:57:51 +0100

    Add add operation tests container.

---
+++ JsonPatchSwiftTests/JPSAddOperationTests.swift
@@ -0,0 +1,9 @@
+//
+//  JPSAddOperationTests.swift
+//  JsonPatchSwift
+//
+//  Created by Dominic Frei on 09/12/2015.
+//  Copyright © 2015 Dominic Frei. All rights reserved.
+//
+
+import Foundation
