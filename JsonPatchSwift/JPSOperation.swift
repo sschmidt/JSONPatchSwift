96cd4e69866ecc68793719b26dc37ed31280e11e
Dominic Frei <dominic@dominicfrei.com>
2015-12-15 10:51:40 +0100

    Fix a typo.

--- JsonPatchSwift/JPSOperation.swift
+++ JsonPatchSwift/JPSOperation.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the JSONPatchSwift open source project.
 //
-// Copyright (c) 2015 EXXEETA AG
+// Copyright (c) 2015 EXXETA AG
 // Licensed under Apache License v2.0
 //
 //
