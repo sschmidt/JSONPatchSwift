0773ae3f5366f510092c95b9fdfa2e4670208f6f
Dominic Frei <dominic@dominicfrei.com>
2016-01-05 16:41:40 +0100

    Add a JsonPointer.

---
+++ JsonPatchSwift/JPSJsonPointer.swift
@@ -0,0 +1,11 @@
+//
+//  JPSJsonPointer.swift
+//  JsonPatchSwift
+//
+//  Created by Dominic Frei on 05/01/2016.
+//  Copyright © 2016 Dominic Frei. All rights reserved.
+//
+
+struct JPSJsonPointer {
+    
+}
