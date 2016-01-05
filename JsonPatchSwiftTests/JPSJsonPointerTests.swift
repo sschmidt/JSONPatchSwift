3e0666110b225d1b2c7a986f7b93c331acf0228c
Dominic Frei <dominic@dominicfrei.com>
2016-01-05 16:44:10 +0100

    Add a test file for the JsonPointer.

---
+++ JsonPatchSwiftTests/JPSJsonPointerTests.swift
@@ -0,0 +1,19 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the JSONPatchSwift open source project.
+//
+// Copyright (c) 2015 EXXETA AG
+// Licensed under Apache License v2.0
+//
+//
+//===----------------------------------------------------------------------===//
+
+import XCTest
+@testable import JsonPatchSwift
+
+// JavaScript Object Notation (JSON) Pointer
+// https://tools.ietf.org/html/rfc6901
+
+class JPSAddOperationTests: XCTestCase {
+    
+}
